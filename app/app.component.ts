4f8b71c99de868abaa4d464107b632a260a8e45c
Javier Gregorio <javier.gregoriom@gmail.com>
2017-02-15 14:27:39 +0000

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'menu.html'
+})
+
+/*@Component({
+  selector: 'my-menu'
+  template: '<div>Esto es el menu</div>'
+})*/
+
+export class AppComponent { }
